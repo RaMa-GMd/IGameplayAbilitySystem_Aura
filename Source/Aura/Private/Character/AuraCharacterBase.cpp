e26479e095e827f7c349090462c464f4a4b8568d
RaMa-GMd <rmdev@coss.org>
2024-10-06 23:56:06 -0400

    Folder "Character"

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Copyright Sollace Studios
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
